da3ecc95d8455162ac079b8e1eb16a5cfc209a13
Antonio Sanchez <antonio@email.ca>
2018-02-06 17:23:46 -0800

    moved updates from experimental

--- matlab/artisynth.m
+++ matlab/artisynth.m
@@ -17,6 +17,7 @@ if (isempty(AP))
     AP = [ AP, AH ];
     setenv ('ARTISYNTH_PATH', AP);    
 end
+setArtisynthClasspath (AH, AP);
 args = [];
 if (isempty(varargin) == 0)
    nargs = length(varargin);
